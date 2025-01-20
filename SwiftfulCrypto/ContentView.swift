6486dd014aaac7ce7f3ec3ac67bc23dd9e359865
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 22:49:47 +0100

    [Chore] Test colors in light and dark modes. - Fix any errors

--- SwiftfulCrypto/ContentView.swift
+++ SwiftfulCrypto/ContentView.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        ZStack {
             Color.theme.background
                 .ignoresSafeArea()
-            VStack {
+            VStack (spacing: 40) {
                 Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
                 Text("Secondary Text Color")
+                    .foregroundColor(Color.theme.secondaryText)
                 Text("Red Color")
+                    .foregroundColor(Color.theme.red)
                 Text("Green Color")
-                
+                    .foregroundColor(Color.theme.green)
             }
+            .font(.headline)
         }
-        .padding()
     }
 }
 
