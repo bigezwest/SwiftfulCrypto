eedb1b8db8b72435e06c6e947511b1a4fcd59f30
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-20 22:40:48 +0100

    [Chore] Add assets - Add colors - Add images

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,20 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Thomas on 1/20/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+}
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let backgbround = Color("BackgroundColor")
+    let greenColor = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
