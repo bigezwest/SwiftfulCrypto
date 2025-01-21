8d322b2e17d6d4e1b59aec8eb25dac35518ddbcc
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-21 17:36:45 +0100

    [Chore] Add CoinRowView SwiftUI View

---
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  SwiftfulCrypto
+//
+//  Created by Thomas on 1/21/25.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
