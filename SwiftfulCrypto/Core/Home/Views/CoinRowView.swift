9bc5fb7cdfb23ab65c84d1d5514d5be7672a4fde
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-21 18:52:30 +0100

    [Refactor] Move left, center, and right columns to views

--- SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
+++ SwiftfulCrypto/Core/Home/Views/CoinRowView.swift
@@ -13,36 +13,12 @@ struct CoinRowView: View {
     
     var body: some View {
         HStack (spacing: 0) {
-            Text("\(coin.rank)")
-                .font(.caption)
-                .foregroundColor(Color.theme.secondaryText)
-                .frame(minWidth: 30)
-            Circle()
-                .frame(width: 30, height: 30)
-            Text(coin.symbol.uppercased())
-                .font(.headline)
-                .padding(.leading, 6)
-                .foregroundColor(Color.theme.accent)
+            leftColumn
             Spacer()
             if showHoldingsColumn {
-                VStack (alignment: .trailing) {
-                    Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
-                        .bold()
-                    Text((coin.currentHoldings ?? 0).asNumberString())
-                }
-                .foregroundColor(Color.theme.accent)
+                centerColumn
             }
-            VStack (alignment: .trailing) {
-                Text(coin.currentPrice.asCurrencyWith6Decimals())
-                    .bold()
-                    .foregroundColor(Color.theme.accent)
-                Text(coin.priceChangePercentage24H?.asPercentStirng() ?? "")
-                    .foregroundColor(
-                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
-                        Color.theme.green : Color.theme.red
-                    )
-            }
-            .frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing)
+            rightColumn
         }
         .font(.subheadline)
         .padding(.trailing, 8)
@@ -54,6 +30,51 @@ struct CoinRowView: View {
 //}
 struct CoinRowView_Previews: PreviewProvider {
     static var previews: some View {
-        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+        Group {
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+            CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+                .previewLayout(.sizeThatFits)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack (spacing: 0) {
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(Color.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+
+        }
+    }
+    private var centerColumn: some View {
+        VStack (alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+        .foregroundColor(Color.theme.accent)
+    }
+    private var rightColumn: some View {
+        VStack (alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(Color.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentStirng() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green : Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing)
     }
 }
