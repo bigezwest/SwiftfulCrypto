f1fea0df494f13db077b4f075c814d7fa3e28659
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-21 05:19:10 +0100

    [Feature] Basic HomeView layout

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // Background Layer ------------------------------------------------
+            Color.theme.background
+                .ignoresSafeArea()
+            // Content Layer ---------------------------------------------------
+            VStack {
+                Text("Header")
+                Spacer()
+            }
+            
+        }
     }
 }
 
