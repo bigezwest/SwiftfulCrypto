bb3c47aa9cd684f64de35300000d9ecd237ed788
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-22 18:38:09 +0100

    [Feature] Add CoinImageService

---
+++ SwiftfulCrypto/Services/CoinImageService.swift
@@ -0,0 +1,32 @@
+//
+//  CoinImageService.swift
+//  SwiftfulCrypto
+//
+//  Created by Thomas on 1/22/25.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService {
+    
+    @Published var image: UIImage? = nil
+    var imageSubscription: AnyCancellable?
+        
+    init(urlString: String) {
+        getCoinImage(urlString: urlString)
+    }
+    private func getCoinImage(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap( { (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
