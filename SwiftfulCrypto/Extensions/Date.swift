fe59195c4b68711715fbb4fd91ad83042796a60b
Thomas <88990664+bigezwest@users.noreply.github.com>
2025-01-29 12:02:05 +0100

    [Feature] Create line chart

---
+++ SwiftfulCrypto/Extensions/Date.swift
@@ -0,0 +1,26 @@
+//
+//  Date.swift
+//  SwiftfulCrypto
+//
+//  Created by Thomas on 1/29/25.
+//
+
+import Foundation
+extension Date {
+    
+    //  2025-01-20T09:11:54.494Z
+    init(coinGeckoString: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let date = formatter.date(from: coinGeckoString) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+    private var shortFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+    func asShortDateString() -> String {
+        return shortFormatter.string(from: self)
+    }
+}
